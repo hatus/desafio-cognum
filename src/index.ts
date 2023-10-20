2d0ecbc9ce5ff051c110350eb249d63b14a5092d
Hatus Níwman <hatusn@gmail.com>
2023-10-20 11:40:24 -0300

    feat: add router to app

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,10 @@
 import express from 'express'
 
+import { router } from './router'
+
 const app = express()
 const PORT = 3000
+
+app.use(router)
 
 app.listen(PORT, () => console.log(`Listening on ${PORT}`))
