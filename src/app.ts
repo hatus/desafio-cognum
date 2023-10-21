f831e8f57f175f1d18f43d4baf1b5c9314b63571
Hatus Níwman <hatusn@gmail.com>
2023-10-20 23:43:24 -0300

    feat: configure apperror

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,23 @@
-import express from 'express'
+import 'express-async-errors'
+import express, { NextFunction, Request, Response } from 'express'
+
+import { router } from './http/router'
+import { AppError } from '@/errors/AppError'
 
 const app = express()
 
-app.use('/', (req, res) => {
-  return res.json({ message: 'hello, world' })
+app.use(express.json())
+app.use(router)
+
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+  if (err instanceof AppError) {
+    return res.status(err.statusCode).json({ message: err.message })
+  }
+
+  res.status(500).json({
+    status: 'error',
+    message: `Internal server error - ${err.message}`,
+  })
 })
 
 export { app }
