288f631e9f3aec9313096ab1babd92460439fa87
Hatus Níwman <hatusn@gmail.com>
2023-10-21 00:36:35 -0300

    feat: create list-all-employee controller

---
+++ src/http/controllers/employees/list-all-employees.ts
@@ -0,0 +1,12 @@
+import { Response } from 'express'
+import { makeListAllEmployeesIdUseCase } from '@/use-cases/factories/make-list-all-employees-use-case'
+
+export class ListAllEmployeesController {
+  async handle(req: Request, res: Response): Promise<Response> {
+    const listAllEmployeesIdUseCase = makeListAllEmployeesIdUseCase()
+
+    const { employees } = await listAllEmployeesIdUseCase.execute()
+
+    return res.status(201).json({ employees })
+  }
+}
