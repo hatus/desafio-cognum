b9905c8a115d207b95fa187259084d449ba6beda
Hatus Níwman <hatusn@gmail.com>
2023-10-21 01:39:43 -0300

    feat: add validation in find-employee-by-id controller

--- src/http/controllers/employees/find-employee-by-id.ts
+++ src/http/controllers/employees/find-employee-by-id.ts
@@ -1,15 +1,20 @@
 import { z } from 'zod'
 import { Response } from 'express'
+
 import { makeFindEmployeeByIdUseCase } from '@/use-cases/factories/make-find-employee-by-id-use-case'
 
 export class FindEmployeeByIdController {
   async handle(req: Request, res: Response): Promise<Response> {
-    const { id } = req.params
+    const findEmployeeByIdParamsSchema = z.object({
+      id: z.string().uuid(),
+    })
+
+    const { id } = findEmployeeByIdParamsSchema.parse(req.params)
 
     const findEmployeeByIdUseCase = makeFindEmployeeByIdUseCase()
 
     const { employee } = await findEmployeeByIdUseCase.execute({ id })
 
-    return res.status(201).json({ employee })
+    return res.status(200).json({ employee })
   }
 }
