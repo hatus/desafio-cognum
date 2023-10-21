02243e0d07c224300ce5487c19237ae21e5aaa07
Hatus Níwman <hatusn@gmail.com>
2023-10-21 02:45:26 -0300

    feat: create populate route

--- src/http/controllers/employees/routes.ts
+++ src/http/controllers/employees/routes.ts
@@ -5,6 +5,7 @@ import { FindEmployeeByIdController } from './find-employee-by-id'
 import { ListAllEmployeesController } from './list-all-employees'
 import { UpdateEmployeeByIdController } from './update-employee-by-id'
 import { DeleteEmployeeByIdController } from './delete-employee-by-id'
+import { PopulateController } from './populate'
 
 const employeesRoutes = Router()
 
@@ -13,8 +14,10 @@ const findEmployeeByIdController = new FindEmployeeByIdController()
 const listAllEmployeesController = new ListAllEmployeesController()
 const updateEmployeeByIdController = new UpdateEmployeeByIdController()
 const deleteEmployeeByIdController = new DeleteEmployeeByIdController()
+const populateController = new PopulateController()
 
 employeesRoutes.post('/', createEmployeeController.handle)
+employeesRoutes.get('/populate', populateController.handle)
 employeesRoutes.get('/', listAllEmployeesController.handle)
 employeesRoutes.get('/:id', findEmployeeByIdController.handle)
 employeesRoutes.patch('/:id', updateEmployeeByIdController.handle)
